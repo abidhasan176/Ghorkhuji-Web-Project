774243271eeb7ce5345463b5c82454bf7ce4d703
MASHRUBA ISLAM <mashruba@MASHRUBAs-MacBook-Air.local>
2026-02-20 16:40:39 +0600

    Registration feature completed

--- ghorkhuji/src/App.jsx
+++ ghorkhuji/src/App.jsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 export default function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 }
