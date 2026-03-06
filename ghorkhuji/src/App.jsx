0fb890ec926527469525ca01a39077ade5dc5d42
MASHRUBA ISLAM <mashrubaislam2012@gmail.com>
2026-03-06 14:12:35 +0600

    Add accessible home, profile page and login update

--- ghorkhuji/src/App.jsx
+++ ghorkhuji/src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import AccessibleHome from "./pages/AccessibleHome";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/accessible-home" element={<AccessibleHome />} />
+      <Route path="/profile" element={<Profile />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
     </Routes>
