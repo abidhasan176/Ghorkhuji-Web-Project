bede1d774042f5ebb03b50b90540939c7031c44a
MASHRUBA ISLAM <mashruba@MASHRUBAs-MacBook-Air.local>
2026-03-06 12:48:19 +0600

    update App.jsx

--- ghorkhuji/src/App.jsx
+++ ghorkhuji/src/App.jsx
@@ -1,1 +1,14 @@
-import { Routes, Route } from "react-router-dom"; import Home from "./pages/Home"; import Register from "./pages/Register"; import Login from "./pages/Login"; export default function App() { return ( <Routes> <Route path="/" element={<Home />} /> <Route path="/register" element={<Register />} /> <Route path="/login" element={<Login />} /> </Routes> ); }
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
